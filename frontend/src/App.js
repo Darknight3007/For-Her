5390cf23f27d29b81898293e2c2f8fdd332162a5
Seekers of Void <khatrigoutam75@gmail.com>
2026-06-19 21:47:47 +0530

    V.A. 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import "@/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Experience from "@/pages/Experience";
 import { Toaster } from "@/components/ui/sonner";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -13,7 +13,9 @@ function App() {
           <Route path="*" element={<Experience />} />
         </Routes>
       </BrowserRouter>
+
       <Toaster theme="dark" position="top-center" richColors />
+      <Analytics />
     </div>
   );
 }
